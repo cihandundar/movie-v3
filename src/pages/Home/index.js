b501baeb0f452b9050a47ec417556e6d22c021ef
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-10-04 14:42:19 +0300

    pages edited

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <main className="main">
+      <section className="section">
+        <div className="section__content"></div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
