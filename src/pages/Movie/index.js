1df297fa7565334fa8c4d493e0652f1cdb0b2728
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-05 22:37:23 +0300

    fix

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -5,7 +5,7 @@ import { fetchMovies } from "../../features/movie/movieSlice";
 
 const Movie = () => {
   const movies = useSelector((state) => state?.movies?.data);
-
+  const isLoading = useSelector((state) => state?.movies?.isLoading);
   const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
   const dispatch = useDispatch();
@@ -16,25 +16,31 @@ const Movie = () => {
 
   return (
     <div className="movie">
-      <div className="movie__title">
-        <div className="movie__title__container">
-          <h1> Populer Movies</h1>
-        </div>
-      </div>
-      <div className="movie__container">
-        {movies?.map((movie) => (
-          <Link key={movie?.id} to={`/movie/${movie.id}`}>
-            <div className="movie__card">
-              <div className="movie__card__img">
-                <img src={IMG_API + movie?.poster_path} alt="" />
-              </div>
-              <div className="movie__card__title">
-                <h3>{movie?.title}</h3>
-              </div>
+      {isLoading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div>
+          <div className="movie__title">
+            <div className="movie__title__container">
+              <h1> Populer Movies</h1>
             </div>
-          </Link>
-        ))}
-      </div>
+          </div>
+          <div className="movie__container">
+            {movies?.map((movie) => (
+              <Link key={movie?.id} to={`/movie/${movie.id}`}>
+                <div className="movie__card">
+                  <div className="movie__card__img">
+                    <img src={IMG_API + movie?.poster_path} alt="" />
+                  </div>
+                  <div className="movie__card__title">
+                    <h3>{movie?.title}</h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
