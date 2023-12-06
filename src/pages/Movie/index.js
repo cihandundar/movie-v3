cb20e5d3cdbc2891a270b447b1509cfa50a035c1
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-06 23:27:25 +0300

    fix

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchMovies, searchMovies } from "../../features/movie/movieSlice";
-
+import { addToFavorites, remove } from "features/movie/favoritesSlice";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 const Movie = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const movies = useSelector((state) => state.movies.data);
+  const favorites = useSelector((state) => state?.favorites?.movieItems);
   const isLoading = useSelector((state) => state.movies.loading);
   const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
@@ -20,6 +22,15 @@ const Movie = () => {
       dispatch(searchMovies(searchTerm));
     } else {
       dispatch(fetchMovies());
+    }
+  };
+
+  const handleAdd = (movie) => {
+    const isSelected = favorites.some((favorite) => favorite.id === movie.id);
+    if (isSelected) {
+      dispatch(remove(movie));
+    } else {
+      dispatch(addToFavorites(movie));
     }
   };
 
@@ -45,16 +56,38 @@ const Movie = () => {
           </div>
           <div className="movie__container">
             {movies?.map((movie) => (
-              <Link key={movie?.id} to={`/movie/${movie.id}`}>
-                <div className="movie__card" key={movie?.id}>
-                  <div className="movie__card__img">
-                    <img src={IMG_API + movie?.poster_path} alt="" />
+              <div style={{ position: "relative" }}>
+                <button
+                  onClick={() => handleAdd(movie)}
+                  style={{
+                    all: "unset",
+                    display: "flex",
+                    cursor: "pointer",
+                    position: "absolute",
+                    right: "10px",
+                    top: "-10px",
+                  }}
+                >
+                  {favorites.some((favorite) => favorite.id === movie.id) ? (
+                    <MdFavorite />
+                  ) : (
+                    <MdFavoriteBorder />
+                  )}
+                </button>
+                <Link key={movie?.id} to={`/movie/${movie.id}`}>
+                  <div className="movie__card" key={movie?.id}>
+                    <div className="movie__card__img">
+                      <img
+                        src={IMG_API + movie?.poster_path}
+                        alt={IMG_API + movie?.poster_path}
+                      />
+                    </div>
+                    <div className="movie__card__title">
+                      <h3>{movie?.title}</h3>
+                    </div>
                   </div>
-                  <div className="movie__card__title">
-                    <h3>{movie?.title}</h3>
-                  </div>
-                </div>
-              </Link>
+                </Link>
+              </div>
             ))}
           </div>
         </div>
