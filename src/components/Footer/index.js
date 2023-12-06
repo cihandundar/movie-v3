3066e48bbe2d6c8673d0d784a886d0d105d34e23
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-07 00:58:37 +0300

    fix

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <a href="https://github.com/cihandundar/movie-v3" target="_blank">
+          Github
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Header from "./Header";
 import Slider from "./Slider";
 import MovieItem from "./MovieItem";
 import Hero from "./Hero";
-export { Header, Slider, MovieItem, Hero };
+import Footer from "./Footer";
+export { Header, Slider, MovieItem, Hero, Footer };
